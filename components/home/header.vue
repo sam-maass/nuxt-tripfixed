db235476fd3ee96531e54572ee62ea76044ae594
Your Name <you@example.com>
2019-04-28 15:11:20 +0200

    feat: add header component

---
+++ components/home/header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <h1 class="header__title">Discover Cheap Flights</h1>
+    <h2 class="header__subtitle">Track Prices - Find Bargains - Relax</h2>
+    <input type="text" class="header__search">
+    <div class="header__info">Find departing from Europe</div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/styles/colors.scss";
+.header {
+  background: $orange;
+  color: $white;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  text-align: center;
+  padding: $padding * 4 $padding * 2;
+  &__title {
+    font-family: "Comfortaa", sans-serif;
+    padding-bottom: $padding/2;
+    font-size: 24px;
+  }
+  &__subtitle {
+    font-family: "Comfortaa", sans-serif;
+    padding-bottom: $padding;
+    font-size: 18px;
+  }
+  &__search {
+    border: 1px solid $white;
+    text-align: center;
+    color: $white;
+    padding: 8px 16px;
+    margin: 16px;
+    border-radius: 12px;
+    background-color: $orange-light;
+    font-size: 24px;
+  }
+}
+</style>
+
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
-    <h1>Hello world!{{test}}</h1>
-    <nuxt-link to="/deals">Deals</nuxt-link>
-    <a href="/deals">Deals</a>
+    <Header></Header>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Header from "../components/home/header.vue";
 export default Vue.extend({
+  components: { Header },
   data: function() {
     return {
       test: "Sam"
